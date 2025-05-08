7fceaaa29779ff89316b276f04102d43f51bdf0c
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-05-07 20:22:37 -0400

    added path

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const app = express();
 const PORT = 3000;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://YOUR-NGROK-URL.ngrok-free.app');
+    const allowedOrigins = ['https://leetcode.com'];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     if (req.method === 'OPTIONS') {
-      return res.sendStatus(200); // respond to preflight
+      return res.sendStatus(200);
     }
     next();
-  });
+});
+
   
 app.use(express.json());
 
